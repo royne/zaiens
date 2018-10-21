3e5f7ca00818b697a89c79d20d7fea03cc41a993
royne <royne.chaves@gmail.com>
2018-10-21 18:03:35 -0500

    sections ok

--- js/app.js
+++ js/app.js
@@ -7,6 +7,37 @@ $(document).ready(function(){
       $('#js_up').fadeOut();
     }
   });
+
+  // section2
+  $(window).scroll(function(){
+    if ($(this).scrollTop() > 200) {
+      $('#section2').css('visibility', 'visible');
+    } else {
+      $('#section2').css('visibility', 'hidden');
+    }
+  });
+
+  // section3
+  $(window).scroll(function(){
+    if ($(this).scrollTop() > 650) {
+      $('#section3').fadeIn();
+    } else {
+      $('#section3').fadeOut();
+      $('.hidden_section3').fadeOut();
+    }
+  });
+
+  // hidden_section3
+  $(window).scroll(function(){
+    if ($(this).scrollTop() > 100) {
+      $('.hidden_section3').css('display', 'block');
+    }
+  });
+  $(window).scroll(function(){
+    if ($(this).scrollTop() > 700) {
+      $('.hidden_section3').fadeOut();
+    }
+  });
 });
 
 // animacion boton top
